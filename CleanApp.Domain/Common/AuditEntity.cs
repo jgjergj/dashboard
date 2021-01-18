6588e03ede89e84a6b85c44a14fb3bd42099c64e
Gjergj Jorgji <gjergj.jorgji@yourspeechfactory.com>
2021-01-18 23:53:40 +0100

    Adding first domain entities

---
+++ CleanApp.Domain/Common/AuditEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanApp.Domain.Common
+{
+    public class AuditEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
---
+++ CleanApp.Domain/Enums/DiscountType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanApp.Domain.Enums
+{
+    public enum DiscountType
+    {
+        Flat,
+        Percentage
+    }
+}
