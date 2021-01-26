0f8a8542207c89d481382b7a3b88b7e0f7ed832f
Gjergj Jorgji <gjergj.jorgji@yourspeechfactory.com>
2021-01-26 18:39:49 +0100

    feat: created first api methd

---
+++ CleanApp.API/Controllers/InvoicesController.cs
@@ -0,0 +1,20 @@
+﻿using CleanApp.Application.Ivoices.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanApp.API.Controllers
+{
+    [Authorize]
+    public class InvoicesController : ApiController
+    {
+        public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
