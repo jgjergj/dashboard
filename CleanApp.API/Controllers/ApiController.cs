53d868eed4df1c4f3b5043d9f629a5da4960338f
Gjergj Jorgji <gjergj.jorgji@yourspeechfactory.com>
2021-04-02 13:44:34 +0200

    chore: updated implementation to newest versions

--- CleanApp.API/Controllers/ApiController.cs
+++ CleanApp.API/Controllers/ApiController.cs
@@ -10,7 +10,7 @@ namespace CleanApp.API.Controllers
     [ApiController]
     public abstract class ApiController : ControllerBase
     {
-        private IMediator _mediator;
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
     }
 }
--- CleanApp.Application/Ivoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
+++ CleanApp.Application/Ivoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
@@ -10,10 +10,9 @@ namespace CleanApp.Application.Ivoices.Validators
 {
     public class MustHaveInvoiceItemPropertyValidator: PropertyValidator
     {
-        public MustHaveInvoiceItemPropertyValidator()
-            :base("Property {PropertyName} should not be an empty list.")
+        protected override string GetDefaultMessageTemplate()
         {
-                
+            return "Property {PropertyName} should not be an empty list.";
         }
 
         protected override bool IsValid(PropertyValidatorContext context)
