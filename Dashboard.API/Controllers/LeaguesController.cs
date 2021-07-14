64e4dc9ee5a2e5786372d0bd58eae728dae96274
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2021-07-14 20:23:43 +0200

    feat (API): added League entity

---
+++ Dashboard.API/Controllers/LeaguesController.cs
@@ -0,0 +1,57 @@
+﻿using Dashboard.Application.Leagues.Commands;
+using Dashboard.Application.Leagues.Queries;
+using Dashboard.Application.Leagues.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dashboard.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class LeaguesController : ApiController
+    {
+        // GET: api/<LeaguesController>
+        [HttpGet]
+        public async Task<IList<LeagueVM>> Get()
+        {
+            return await Mediator.Send(new GetLeaguesQuery());
+        }
+
+        // GET api/<LeaguesController>/5
+        [HttpGet("{id}")]
+        public async Task<LeagueVM> Get(int id)
+        {
+            return await Mediator.Send(new GetLeagueQuery(id));
+        }
+
+        // POST api/<LeaguesController>
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateLeagueCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        // PUT api/<LeaguesController>
+        [HttpPut]
+        public async Task<ActionResult<Unit>> Update(int id, UpdateLeagueCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            return await Mediator.Send(command);
+        }
+
+        // DELETE api/<LeaguesController>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(int id)
+        {
+            return await Mediator.Send(new DeleteLeagueCommand { Id = id });
+        }
+    }
+}
