2c0ce336ff9be044d0fb96f8534f9e0d626c3d91
User-PC\User <renikaziu.rk@gmail.com>
2021-07-21 15:57:30 +0200

    feat (Domain): added Team entity

---
+++ Dashboard.Domain/Entities/Team.cs
@@ -0,0 +1,16 @@
+﻿using Dashboard.Domain.Common;
+
+namespace Dashboard.Domain.Entities
+{
+    public class Team : AuditEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StateId { get; set; }
+        public State State { get; set; }
+        public int SportId { get; set; }
+        public Sport Sport { get; set; }
+        public int LeagueId { get; set; }
+        public League League { get; set; }
+    }
+}
