36969d346148414318704df3914d20906db96c93
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2021-07-23 00:24:21 +0200

    bugfix (Domain): removed client reference to Operator entity

--- Dashboard.Domain/Entities/Operator.cs
+++ Dashboard.Domain/Entities/Operator.cs
@@ -7,14 +7,14 @@ namespace Dashboard.Domain.Entities
 {
     public class Operator : AuditEntity
     {
-        public Operator()
-        {
-            this.Clients = new List<Client>();
-        }
+        //public Operator()
+        //{
+        //    this.Clients = new List<Client>();
+        //}
         public int Id { get; set; }
         public string Name { get; set; }
         public double Balance { get; set; }        
-        public List<Client> Clients { get; set; }
+        //public List<Client> Clients { get; set; }
 
         //public int DepartmentId { get; set; }
         //public Department DepartmentId { get; set; }
