15d097efc388c9185482b2c55f050d40482f9df3
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2021-07-23 00:33:42 +0200

    chore (Infrastructure): added migration for PaymentAccount entity

---
+++ Dashboard.Infrastructure/Migrations/20210722223229_AddedPaymentAccountEntity.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Dashboard.Infrastructure.Migrations
+{
+    public partial class AddedPaymentAccountEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Operators_OperatorId",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OperatorId",
+                table: "Clients",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateTable(
+                name: "PaymentAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CompanyId = table.Column<int>(type: "integer", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    StateId = table.Column<int>(type: "integer", nullable: false),
+                    StatusId = table.Column<int>(type: "integer", nullable: false),
+                    Balance = table.Column<double>(type: "double precision", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Surname = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    DocumentExpiry = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    LastModifiedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentAccounts_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PaymentAccounts_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PaymentAccounts_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentAccounts_CompanyId",
+                table: "PaymentAccounts",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentAccounts_StateId",
+                table: "PaymentAccounts",
+                column: "StateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentAccounts_StatusId",
+                table: "PaymentAccounts",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Operators_OperatorId",
+                table: "Clients",
+                column: "OperatorId",
+                principalTable: "Operators",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Operators_OperatorId",
+                table: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "PaymentAccounts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OperatorId",
+                table: "Clients",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Operators_OperatorId",
+                table: "Clients",
+                column: "OperatorId",
+                principalTable: "Operators",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
