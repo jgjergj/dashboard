715eb43e224ffba2f38249eac6bde297fae3bacc
Gjergj Jorgji <gjergj.jorgji@gmail.com>
2021-09-21 01:16:01 +0200

    feat (Application): added Utils method to use for filtering

---
+++ Dashboard.Application/Common/Utils/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Dashboard.Application.Common.Utils
+{
+    public class Utils
+    {
+        public static Expression<Func<T, bool>> CreatePredicate<T>(string columnName, object searchValue)
+        {
+            var xType = typeof(T);
+            var x = Expression.Parameter(xType, "x");
+            var column = xType.GetProperties().FirstOrDefault(p => p.Name == columnName);
+            if (column.PropertyType == typeof(int))
+            {
+                searchValue = Convert.ToInt32(searchValue);
+            }
+
+            var body = column == null
+                ? (Expression)Expression.Constant(true)
+                : Expression.Equal(
+                    Expression.PropertyOrField(x, columnName),
+                    Expression.Constant(searchValue));
+
+            return Expression.Lambda<Func<T, bool>>(body, x);
+        }
+    }
+}
